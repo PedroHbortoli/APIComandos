7568d706ae592545882b33c152a183c417d1bad2
Pedro H. Bortoli <bortolipedroh@gmail.com>
2023-09-15 18:10:05 -0300

    criamos rotas http de crud

--- src/server.js
+++ src/server.js
@@ -7,5 +7,21 @@ const dotenv = require('dotenv').config();
 // definir a porta do servidor 
 const PORT = process.env.PORT || 3001;
 
+app.get('/api', (request, response) => (
+    response.send('Retorno de lista de informações do bando de dados')
+));
+
+app.post('/api', (request, responde) =>(
+    responde.send('Metodo utilizado para salvar informações')
+));
+
+app.put('/api/:id', (request, responde) =>(
+    responde.send('Metodo utilizado para editar informações')
+));
+
+app.delete('/api/:id', (request, responde) =>(
+    responde.send('Metodo utilizado para deletar informações')
+));
+
 // testar servidor 
 app.listen(PORT, () => console.log(`Running at port ${PORT}`))
