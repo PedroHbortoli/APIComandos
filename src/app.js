5bc30bb3e8449a47f479c6269524e132a9457ac4
Pedro H. Bortoli <bortolipedroh@gmail.com>
2023-11-03 18:06:47 -0300

    utilizando sql alunos

--- src/app.js
+++ src/app.js
@@ -6,12 +6,16 @@ const app = express();
 
 //Importar as rotas para serem executadas na aplicação
 const crudRouter = require('./routes/crudRouter');
+//Importar as rotas para serem executadas na aplicação
+const alunosRouter = require('./routes/alunosRouter');
 
 // Importar pacote dotenv
 const dotenv = require('dotenv').config();
 
 // Habilitar a utilização do crudRauter
 app.use('/api',crudRouter);
+// Habilitar a utilização do crudRauter
+app.use('/api',alunosRouter);
 
 // Setar a porta do servidor, a partir do arquivo .env
 app.set('port', process.env.PORT);
---
+++ src/routes/alunosRouter.js
@@ -0,0 +1,8 @@
+//
+const { Router } = require('express');
+const router     = Router();
+const listarUsuarios = require('../controllers/alunosController')
+
+router.get('/alunos', listarUsuarios);
+
+module.exports = router;
